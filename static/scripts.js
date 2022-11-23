9437706b40989c345681d44dba916298fe9d4892
Lyndon <96540787+Ethereal-Slime@users.noreply.github.com>
2022-11-23 17:52:50 +0000

    done backend

--- static/scripts.js
+++ static/scripts.js
@@ -2,22 +2,39 @@ const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
 $(document).ready(() => {
-  $(function() {
-      var $stocks = $('#values');
-      $.ajax({
-          type: 'GET',
-          url: '/stocks/FB',
-          success: function(stock) {
-              $.each(stock, function(index, stock) {
-                  $stocks.append('<p> value: '+ stock.value + ' time stamp: ' + stock.timestamp+ '</p>')
+  $(function(){
+    var $stocks = $('#values');
+    $.ajax({
+      type: 'GET',
+      url: '/stocks',
+      success: function(stock_main){
+        $.each(stock_main,function(index,stock_main){
+          let lenght_of_arr = stock_main.length;
+          //$stocks.append'<p> value: '+ stock.value + ' time stamp: ' + stock.timestamp+ '</p>'
+          for(let i=0; i<lenght_of_arr;i++){
+            $(document).ready(() => {
+              $(function(){
+                $.ajax({
+                  type:'GET',
+                  url:'/stocks/' + stock_main[i],
+                  success: function(stock_info){
+                    $.each(stock_info, function(index,stock_info){
+                      let id = index+1;
+                      $stocks.append('<p> stock id: '+id+ ' name: ' +stock_main[i]+ " value: " + stock_info.value+ " timeStamp: " +stock_info.timestamp+'</p>')
+                    })
+
+                    
+                  }
+                })
               })
-              
-              setTimeout(() => {
-                $('#spinner').removeAttr('id');
-              }, 2000);
-              
+            })
           }
-      })
+        })
+        setTimeout(() => {
+          $('#spinner').removeAttr('id');
+        }, 2000);
+      }
+    })
   })
 });
 
