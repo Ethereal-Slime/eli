9917d7c3584d732c0458b2a79446e68d9f2d5aec
Lyndon <96540787+Ethereal-Slime@users.noreply.github.com>
2022-11-24 16:05:37 +0000

    drawing some lines

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,30 @@ const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
 /* unix time for timestamp */
+
+function drawLine (start, end, style) {
+  ctx.beginPath()
+  ctx.strokeStyle = style || 'black'
+  ctx.moveTo(...start)
+  ctx.lineTo(...end)
+  ctx.stroke()
+}
+
+function drawTriangle (apex1, apex2, apex3) {
+  ctx.beginPath()
+  ctx.moveTo(...apex1)
+  ctx.lineTo(...apex2)
+  ctx.lineTo(...apex3)
+  ctx.fill()
+}
+
+drawLine([50, 50], [50, 550])
+drawTriangle([35, 50], [65, 50], [50, 35])
+
+drawLine([50, 550], [950, 550])
+drawTriangle([950, 535], [950, 565], [965, 550])
+
+
 $(document).ready(() => {
   $(function(){
     var $stocks = $('#values');
@@ -29,6 +53,9 @@ $(document).ready(() => {
                       var new_name = stock_main[i]
                       //$.stocks_main[i].append('<p> stock_id: '+id+  " value: " + stock_info.value+ " timeStamp:     " + new Date(stock_info.timestamp)+'</p>')
                       $stocks.append('<p> stock_id: '+id+  " value: " + stock_info.value+ " timeStamp:     " + new Date(stock_info.timestamp)+'</p>')
+                      //ctx.lineTo([stock_info,50],[90*id,550])
+                      drawLine([50+90*(id-1),550],[950,50])
+                      //drawLine([50+90*(id-1),550],[950,50])
                       //$stocks.append('<p>' + $.id=stock_main[i]+ " stock_id: " +id+ " value: "+stock_info.value+ " timestamp: "+ stock_info.timestamp + '</p>')
                     })
                   }
@@ -46,25 +73,3 @@ $(document).ready(() => {
     })
   })
 });
-
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
-}
-
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
-}
-
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
-
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
