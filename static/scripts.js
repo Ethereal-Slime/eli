20823a23f9ab9d6efb5460ecb3cc1d5968ba4b74
Lyndon <96540787+Ethereal-Slime@users.noreply.github.com>
2022-11-24 15:22:44 +0000

    attemp to add id

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
+/* unix time for timestamp */
 $(document).ready(() => {
   $(function(){
     var $stocks = $('#values');
@@ -10,17 +11,25 @@ $(document).ready(() => {
       success: function(stock_main){
         $.each(stock_main,function(index,stock_main){
           let lenght_of_arr = stock_main.length;
+          
           //$stocks.append'<p> value: '+ stock.value + ' time stamp: ' + stock.timestamp+ '</p>'
           for(let i=0; i<lenght_of_arr;i++){
             $(document).ready(() => {
               $(function(){
                 $.ajax({
+                  
                   type:'GET',
                   url:'/stocks/' + stock_main[i],
+                  
                   success: function(stock_info){
+                    $stocks.append('<p> Name: ' + stock_main[i]+'</p>')
                     $.each(stock_info, function(index,stock_info){
+                      
                       let id = index+1;
-                      $stocks.append('<p> stock id: '+id+ ' name: ' +stock_main[i]+ " value: " + stock_info.value+ " timeStamp: " +stock_info.timestamp+'</p>')
+                      var new_name = stock_main[i]
+                      //$.stocks_main[i].append('<p> stock_id: '+id+  " value: " + stock_info.value+ " timeStamp:     " + new Date(stock_info.timestamp)+'</p>')
+                      $stocks.append('<p> stock_id: '+id+  " value: " + stock_info.value+ " timeStamp:     " + new Date(stock_info.timestamp)+'</p>')
+                      //$stocks.append('<p>' + $.id=stock_main[i]+ " stock_id: " +id+ " value: "+stock_info.value+ " timestamp: "+ stock_info.timestamp + '</p>')
                     })
                   }
                 })
